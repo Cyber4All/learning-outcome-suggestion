d6aca0a790606d36b0d0ad4c426bf0160c4ca584
Gustavus Shaw II <gshaw1997@gmail.com>
2018-10-10 14:50:51 -0400

    :recycle: Reorganize types

--- src/types/reason.ts
+++ src/types/reason.ts
@@ -1,6 +1,6 @@
 export enum REASON {
-  METHOD_NOT_IMPLEMENTED = 501,
   BAD_REQUEST = 400,
+  METHOD_NOT_IMPLEMENTED = 501,
 }
 export enum GENERIC_REASON {
   UNEXPECTED_ERROR = 500,
@@ -11,11 +11,11 @@ export type GenericReason = GENERIC_REASON;
 export enum DRIVER_REASON {
   RESOURCE_NOT_FOUND = 404,
 }
-export type DriverReason = GenericReason | DRIVER_REASON;
+export type DriverReason = GenericReason | DRIVER_REASON | REASON;
 
 export enum SERVICE_REASON {
   NOT_AUTHORIZED = 403,
 }
-export type ServiceReason = GenericReason | SERVICE_REASON;
+export type ServiceReason = GenericReason | SERVICE_REASON | REASON;
 
-export type Reason = REASON | GenericReason | DriverReason | ServiceReason;
+export type Reason = GenericReason | DriverReason | ServiceReason;
