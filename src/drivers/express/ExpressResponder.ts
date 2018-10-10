72c08a29a9221498d5773f84bb8668614d018ae4
Gustavus Shaw II <gshaw1997@gmail.com>
2018-10-10 12:46:14 -0400

    :recycle: Change error parameters to take ClientError

--- src/drivers/express/ExpressResponder.ts
+++ src/drivers/express/ExpressResponder.ts
@@ -1,15 +1,13 @@
 import { Responder } from '../../interfaces/interfaces';
 import { Response } from 'express';
+import { ClientError } from '../../types';
 export class ExpressResponder implements Responder {
   constructor(private res: Response) {}
   sendOperationSuccess(): void {
     this.res.sendStatus(200);
   }
-  sendOperationError(
-    error: string = 'Server error encounter.',
-    status: number = 400,
-  ): void {
-    this.res.status(status).send(error);
+  sendOperationError(error: ClientError): void {
+    this.res.status(error.status).send(error.message);
   }
   sendObject(object: any): void {
     this.res.status(200).send(object);
--- src/interfaces/Responder.ts
+++ src/interfaces/Responder.ts
@@ -1,7 +1,8 @@
 import { Response } from 'express';
+import { ClientError } from '../types';
 export interface Responder {
   sendOperationSuccess(): void;
-  sendOperationError(error?: string, status?: number): void;
+  sendOperationError(error: ClientError): void;
   sendObject(object: any): void;
 
   writeStream(): Response;
