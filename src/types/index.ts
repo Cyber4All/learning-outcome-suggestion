c222418a39bc1fc7ac95ca72411dfbce0e55f898
Gustavus Shaw II <gshaw1997@gmail.com>
2018-10-10 12:43:41 -0400

    :sparkles: Export types

---
+++ src/types/index.ts
@@ -0,0 +1,27 @@
+import {
+  REASON,
+  Reason,
+  GENERIC_REASON,
+  GenericReason,
+  DRIVER_REASON,
+  DriverReason,
+  SERVICE_REASON,
+  ServiceReason,
+} from './reason';
+import { DriverError } from './DriverError';
+import { ServiceError } from './ServiceError';
+import { ClientError } from './ClientError';
+
+export {
+  REASON,
+  Reason,
+  GENERIC_REASON,
+  GenericReason,
+  DRIVER_REASON,
+  DriverReason,
+  SERVICE_REASON,
+  ServiceReason,
+  DriverError,
+  ServiceError,
+  ClientError,
+};
